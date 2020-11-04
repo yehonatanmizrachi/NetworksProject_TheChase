ba34cca208885a1792371a497355f4352893aa5d
YehonatanMiz <ymizrachi5@gmail.com>
2020-11-04 09:40:01 +0200

    moved server into manager

---
+++ backend/Client.py
@@ -0,0 +1,4 @@
+class Client:
+    def __init__(self, socket, address):
+        self.socket = socket
+        self.address = address
---
+++ backend/GameManager.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+from Client import *
+
+
+class GameManager:
+    def __init__(self):
+        ########################################
+        # server settings
+        self.port = 5050
+        self.ip = "192.168.100.6"
+        self.address = (self.ip, self.port)
+        # count how many active clients there is
+        self.active_clients = 0
+        # the size of the msg in bytes.
+        self.msg_size = 1024
+        self.format = 'utf-8'
+        self.disconnect_message = "!DISCONNECT"
+        self.max_active_client = 3
+        # server socket
+        # AF_INET -> address family IPV4, SOCK_STREAM -> protocol TCP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(self.address)
+        ########################################
+
+    # start listening to requests(a thread that always runs in the background)
+    # and open a new thread for each client
+    def get_new_clients(self):
+        print("server is listening...")
+        self.socket.listen()
+        while True:
+            # wait for a new connection
+            client_socket, client_address = self.socket.accept()
+            if self.active_clients < self.max_active_client:
+                new_client = Client(client_socket, client_address)
+                thread = threading.Thread(target=self.handel_client, args=(new_client,))
+                thread.start()
+                self.active_clients += 1
+                print(f"active connections: {self.active_clients}")
+            else:
+                print("server is full. request denied!")
+                client_socket.close()
+
+    # each client runs on a different thread
+    def handel_client(self, client):
+        print("connected successfully.")
+        connected = True
+        while connected:
+            # wait for a new massage
+            msg = client.socket.recv(self.msg_size).decode(self.format)
+            if msg == self.disconnect_message:
+                connected = False
+            print(f"[{client.address}] {msg}")
+            client.socket.send("Msg received".encode(self.format))
+        client.socket.close()
+
+
+my_manager = GameManager()
+my_manager.get_new_clients()
